13587f0fb32e353f797dda61ecd7471a2276e5e2
shurilla <shurilla@gmail.com>
2021-09-23 14:02:14 +0300

    new add

--- main.go
+++ main.go
@@ -13,4 +13,23 @@ import (
 )
 
 // функция логирования ошибок
-func errorLoger()
+func errorLoger(errorToaddFile error, msgtoErrorLogFile string) {
+	logFileWrite, err := os.OpenFile("error.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalln("НЕ могу создать файл лога")
+	}
+	log.SetOutput(logFileWrite)
+	log.Printf("%s:%s", msgtoErrorLogFile, errorToaddFile)
+}
+
+//структура файла конфигурации
+type ConfigYaml struct {
+	Host          string `yaml:"host"`
+	Port          string `yaml:"port"`
+	Login         string `yaml:"login"`
+	Password      string `yaml:"passwd"`
+	QueueName     string `yaml:"queueName"`
+	QueueMessages int    `yaml:"queueMessages"`
+	QueueCount    int    `yaml:"queueCount"`
+	Threading     int    `yaml:"threading"`
+}
